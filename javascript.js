4f3fc9c7763558711e8aae9fe7c0f1c9d0503a9e
Josue <josuedu96@gmail.com>
2022-09-21 09:56:41 -0500

    Give life to scoring buttons

---
+++ javascript.js
@@ -0,0 +1,39 @@
+let homeScore = 0
+let guestScore = 0
+document.getElementById('home-points').textContent = homeScore
+document.getElementById('guest-points').textContent = guestScore
+
+
+//home score
+
+function homePlusOne() {
+    homeScore += 1
+    document.getElementById('home-points').textContent = homeScore
+}
+
+function homePlusTwo() {
+    homeScore += 2
+    document.getElementById('home-points').textContent = homeScore
+}
+
+function homePlusThree() {
+    homeScore += 3
+    document.getElementById('home-points').textContent = homeScore
+}
+
+//guest score
+
+function guestPlusOne() {
+    guestScore += 1
+    document.getElementById('guest-points').textContent = guestScore
+}
+
+function guestPlusTwo() {
+    guestScore += 2
+    document.getElementById('guest-points').textContent = guestScore
+}
+
+function guestPlusThree() {
+    guestScore += 3
+    document.getElementById('guest-points').textContent = guestScore
+}
