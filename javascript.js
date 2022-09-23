a10b97b45abf631bae0a30114b5ce3fa51c5ba67
Josue <josuedu96@gmail.com>
2022-09-23 15:40:36 -0500

    Make foul buttons work

--- javascript.js
+++ javascript.js
@@ -3,6 +3,13 @@ let guestScore = 0
 let homePointsEl = document.getElementById('home-points')
 let guestPointsEl = document.getElementById('guest-points')
 
+let homeFoulEl = document.getElementById('home-foul-points')
+let guestFoulEl = document.getElementById('guest-foul-points')
+let homeFouls = 0
+let guestFouls = 0
+
+
+
 //home score
 function homePoints(points) {
     homeScore += points
@@ -17,10 +24,17 @@ function guestPoints(points) {
     colorLeader()
 }
 
-// function addPoints(team, points) {
-//     team += points
-//     colorLeader()
-// }
+//foul score
+function foul(foulTeam) {
+   if (foulTeam == 'home') {
+        homeFouls++
+        homeFoulEl.textContent = homeFouls 
+   } else if (foulTeam == 'guest') {
+        guestFouls++
+        guestFoulEl.textContent = guestFouls
+   }
+
+}
 
 //Highlight the leader
 function colorLeader() {
@@ -40,7 +54,16 @@ if (homeScore > guestScore) {
 function newGame() {
     homeScore = 0
     guestScore = 0
+    homeFouls = 0
+    guestFouls = 0
     guestPointsEl.textContent = guestScore
     homePointsEl.textContent = homeScore
+    homeFoulEl.textContent = homeFouls
+    guestFoulEl.textContent = guestFouls
     colorLeader()
 }
+
+/*// function addPoints(team, points) {
+//     team += points
+//     colorLeader()
+// }*/
